9c91f5fa2488bdb450beb3727dc8a5442baac276
Rzesisa <tongyang1786@gmail.com>
2018-06-07 18:08:16 +0800

    admin fix

--- flaskprj/admin.py
+++ flaskprj/admin.py
@@ -1,14 +1,20 @@
 from flask_admin.contrib import sqla
 from flask_admin import AdminIndexView
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 class BlogModelView(sqla.ModelView):
     def is_accessible(self):
         # return current_user.is_authenticated
-        return current_user.is_accessible
+        try:
+            return current_user.is_accessible
+        except:
+            return False
 
 
 class BlogAdminIndexView(AdminIndexView):
     def is_accessible(self):
-        return current_user.is_accessible
+        try:
+            return current_user.is_accessible
+        except:
+            return False
     
